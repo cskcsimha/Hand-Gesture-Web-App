468e510003d46df462b21615d75c67158646e453
cskcsimha <92619094+cskcsimha@users.noreply.github.com>
2022-02-20 12:44:34 +0530

    Add files via upload

--- Hand Gesture Web App/Hand Gesture Web App/main.js
+++ Hand Gesture Web App/Hand Gesture Web App/main.js
@@ -26,3 +26,29 @@ function speak(){
     var utterThis=new SpeechSynthesisUtterance(speak_data_1);
     synth.speak(utterThis);
 }
+
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML=results[0].label;
+        prediction_1=results[0].label;
+        speak();
+        if(results[0].label=="Amazing"){
+            document.getElementById("update_gesture").innerHTML="&#128076;";
+        }
+        if(results[0].label=="Victory"){
+            document.getElementById("update_gesture").innerHTML="&#9996;";
+        }
+        if(results[0].label=="Best"){
+            document.getElementById("update_gesture").innerHTML="&#128077;";
+        }
+    }
+}
